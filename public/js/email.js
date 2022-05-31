e8b09f1a5eff74f252812510a8bc5e083ab5afd0
Sarah <pappas.sarah@gmail.com>
2022-05-31 14:23:23 -0700

    formatting fixes

--- public/js/email.js
+++ public/js/email.js
@@ -1,8 +1,8 @@
 function displayEmail() {
-	var el = document.getElementById("js-email");
-	el.textContent = atob("cGFwcGFzLnNhcmFoQGdtYWlsLmNvbQ==");
+    var el = document.getElementById("js-email");
+    el.textContent = atob("cGFwcGFzLnNhcmFoQGdtYWlsLmNvbQ==");
 }
 
 document.body.onload = function () {
-	setTimeout(displayEmail, 1000);
+    setTimeout(displayEmail, 1000);
 }
